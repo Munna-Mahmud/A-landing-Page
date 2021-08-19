710caf2cd89584abac6ae1c293efa4892686499c
Munna-Mahmud <learnermunna@gmail.com>
2021-08-20 00:22:12 +0600

    change color by using js

---
+++ app.js
@@ -0,0 +1,9 @@
+const topPlayersTitle= document.getElementById('top-players-title');
+topPlayersTitle.style.color='orange';
+
+const blogsTitle=document.getElementById('blogs-title');
+blogsTitle.style.color='orange';
+//bg
+
+const bgColor=document.getElementById('bg-color');
+bgColor.style.backgroundColor='rgba(255, 0, 0, 0.2)';
